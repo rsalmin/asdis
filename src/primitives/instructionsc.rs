6298e0b536fa25d987ca5459bbce2278570c508e
Roman Salmin <Roman.Salmin@gmail.com>
2021-03-07 20:37:51 +0300

    added 32C IDS description, but it need to be reworked

--- src/primitives/instructionsc.rs
+++ src/primitives/instructionsc.rs
@@ -5,7 +5,7 @@ use std::hash::Hash;
 use super::primitives::*;
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
-pub enum InstructionCFmt { CR, CI, CSS, CIW, CL, CS, CA, CB, CJ }
+pub enum InstructionCFmt { CR, CI, CSS, CIW, CL, CS, CA, CB, CB1, CB2, CJ, CCST }
 
 pub enum InstructionC {
     CR  { func4 : Func4, rdrs1 : Register, rs2 : Register,  opcode : OpcodeC },
@@ -16,5 +16,8 @@ pub enum InstructionC {
     CS  { func3 : Func3, imm : ux::u5, rs1prime : Register3, rs2prime : Register3, opcode : OpcodeC },
     CA  { func6 : Func6,  rdrs1prime : Register3,  func2 : Func2, rs2prime : Register3, opcode : OpcodeC },
     CB  { func3 : Func3, offset : u8, rs1prime : Register3, opcode : OpcodeC },
+    CB1  { func3 : Func3, func2 : Func2, offset : u8, rs1prime : Register3, opcode : OpcodeC },
+    CB2  { func3 : Func3, func2 : Func2, offset : u8, rs1prime : Register3, opcode : OpcodeC },
     CJ   { func3 : Func3, target : ux::u11,  opcode : OpcodeC },
+    CCST { cst : u16 },
 }
